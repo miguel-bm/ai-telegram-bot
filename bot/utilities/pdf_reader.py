6a044959c1991ab12af8771f9c7ae407a033d552
Miguel Blanco Marcos <miguel.blanco.marcos@gmail.com>
2023-04-16 20:50:15 +0200

    chore: delete obsolete code

--- bot/utilities/pdf_reader.py
+++ bot/utilities/pdf_reader.py
@@ -9,9 +9,3 @@ def extract_text_from_pdf(pdf_path: Path):
         for page in reader.pages:
             pages_text.append(page.extract_text())
     return "\n\n".join(pages_text)
-
-
-if __name__ == "__main__":
-    pdf_path = Path("/Users/miguel/Downloads/s41586-023-05781-7.pdf")
-    text = extract_text_from_pdf(pdf_path)
-    print(len(text.split()))
